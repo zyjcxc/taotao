45e5cbed8203bc4ebf6db0369269613a0773a70f
zyjcxc <174866687@qq.com>
2018-06-05 16:22:47 +0800

    1.后台内容管理 2.前台大广告位展示 3.Httpclent工具类

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.taotao.portal.controller;
 
+import com.taotao.portal.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private ContentService contentService;
+
     @RequestMapping("/index")
-    public String showIndex() {
+    public String showIndex(Model model) {
+        String adJson = contentService.getContentList();
+        model.addAttribute("ad1", adJson);
+
         return "index";
     }
 
